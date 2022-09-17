8092a0a15966ee68702e11640e5506308effc26f
Sergey <@>
2022-09-17 15:57:41 +0300

    Добавил функцию умножения, добавил файл работы со строкой

--- first/nums.py
+++ first/nums.py
@@ -4,8 +4,12 @@ def plus(a, b):
 def minus(a, b):
     return a - b
 
+def mult(a, b):
+    return a * b
+
 if __name__ == '__main__':
     a = 10
     b = 2
     print(plus(a, b))
     print(minus(a, b))
+    print(mult(a, b))
